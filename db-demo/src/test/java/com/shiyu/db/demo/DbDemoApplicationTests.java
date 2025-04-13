cef4684502ef45bc5c7b2fd0bcacd217ff67ceb1
shiyu <876703634@qq.com>
2025-04-13 21:11:13 +0800

    init

---
+++ db-demo/src/test/java/com/shiyu/db/demo/DbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiyu.db.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
